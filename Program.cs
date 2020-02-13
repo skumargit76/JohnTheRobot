c7d6bdf37da9c91cf0f46fa68dd50a6b308188df
skumargit76 <s.kumar.mcs@gmail.com>
2020-02-14 06:53:04 +0800

    commit1

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JohnTheRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Humanoid(new Dancing());
+            Console.WriteLine(john.ShowSkill()); //print dancing
+
+            var alex = new Humanoid(new Cooking());
+            Console.WriteLine(alex.ShowSkill());//print cooking
+
+            var bob = new Humanoid();
+            Console.WriteLine(bob.ShowSkill());//print no skill is defined
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+        //Implimentation of Humanoid class
+        interface IHumanoid
+        {
+            string Skill { get; set; }
+        }
+        //Humanoid
+        class Humanoid : IHumanoid
+        {
+            public string Skill { get; set; }         
+            public Humanoid(IHumanoid Obj)
+            {
+                Skill = Obj.Skill;
+            }
+            public Humanoid()
+            {
+                Skill = "No skill is defined";
+            }
+            public string ShowSkill()
+            {
+                return Skill;
+            }
+        }
+        //dancing
+        class Dancing : IHumanoid
+        {
+            public string Skill { get; set; }
+            public Dancing()
+            {
+                Skill = "Dancing";
+            }
+        }
+        //cooking
+        class Cooking : IHumanoid
+        {
+            public string Skill { get; set; }
+            public Cooking()
+            {
+                Skill = "Cooking";
+            }
+        }
+
+    }
+}
